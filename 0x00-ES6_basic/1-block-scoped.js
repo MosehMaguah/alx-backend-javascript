659deeb67cc9377c4240a5b3dc062a35ae2a661d
moxi <mosehmaguah@gmail.com>
2023-09-25 18:39:47 +0300

    block scopes

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,10 +1,10 @@
 export default function taskBlock(trueOrFalse) {
-  const var task = false;
-  const var task2 = true;
+  const task = false;
+  const task2 = true;
 
   if (trueOrFalse) {
-    const var task = true;
-    const var task2 = false;
+    const task = true;
+    const task2 = false;
   }
 
   return [task, task2];
