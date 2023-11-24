479b79f10185358808abcaced00cc9469bac7209
mosehmaguah <you@example.com>
2023-11-24 15:09:56 +0300

    nodejs basics

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,11 +1,5 @@
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
-if (process.stdin.isTTY) {
-  process.stdin.on('data', (data) => {
-    process.stdout.write(`Your name is: ${data.toString()}`);
-    process.exit();
-  });
-} else {
   process.stdin.on('data', (data) => {
     process.stdout.write(`Your name is: ${data.toString()}`);
     process.exit();
@@ -13,4 +7,3 @@ if (process.stdin.isTTY) {
   process.on('exit', () => {
     process.stdout.write('This important software is now closing\n');
   });
-}
