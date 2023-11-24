4c097ede3292da77ec6c93e35d7bcbec3657ed90
mosehmaguah <you@example.com>
2023-11-24 14:53:52 +0300

    nodejs basics

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,16 +1,16 @@
-process.stdout.write('Welcome to Holberton School, what is your name?\n');
+const readline = require('readline');
 
-if (process.stdin.isTTY) {
-  process.stdin.on('data', (data) => {
-    process.stdout.write(`Your name is: ${data.toString()}`);
-    process.exit();
-  });
-} else {
-  process.stdin.on('data', (data) => {
-    process.stdout.write(`Your name is: ${data.toString()}`);
-    process.exit();
-  });
-  process.on('exit', () => {
-    process.stdout.write('This important software is now closing\n');
-  });
-}
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+rl.question('Welcome to Holberton School, what is your name? ', (name) => {
+  console.log(`Your name is: ${name}`);
+
+  rl.close();
+});
+
+rl.on('close', () => {
+  console.log('This important software is now closing');
+});
